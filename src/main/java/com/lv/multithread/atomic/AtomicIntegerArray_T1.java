ce917ffa93ed77bef540201e7fd974e90157b825
lvjn <ljn13326@163.com>
2022-02-13 16:28:30 +0800

    duoxianc

--- src/main/java/com/lv/multithread/atomic/AtomicIntegerArray_T1.java
+++ src/main/java/com/lv/multithread/atomic/AtomicIntegerArray_T1.java
@@ -1,8 +1,19 @@
-package com.lv.multithread.atomic;/**
- * @description: TODO 
+package com.lv.multithread.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+/**
  * @author lvjn
- * @date 2022/1/24 20:10
  * @version 1.0
+ * @description: TODO
+ * @date 2022/1/24 20:10
  */
 public class AtomicIntegerArray_T1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicIntegerArray array = new AtomicIntegerArray(new int[] { 0, 0 });
+        System.out.println(array);
+        System.out.println(array.getAndAdd(1, 2));
+        System.out.println(array);
+    }
 }
